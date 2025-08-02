07b51b2b20976f1d8fb4c702e27780ab41290110
fise15 <sigmasigmovich@gmail.com>
2025-08-02 18:40:18 +0500

    update fina;

--- app/main.py
+++ app/main.py
@@ -16,9 +16,11 @@ DATABASE_URL = "sqlite+aiosqlite:///./recipes.db"
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
+
 # Define Declarative Base class after engine and session setup
 class Base(DeclarativeBase):
     pass
+
 
 app = FastAPI(
     title="Recipe Book API",
