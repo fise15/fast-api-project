f05e5812dd28b8414c990d0be0fb3b0751e9f29b
fise15 <sigmasigmovich@gmail.com>
2025-08-02 18:46:35 +0500

    finalist

---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+
+
+
+import asyncio
+import pytest
+
+from app.main import engine, Base
+
+@pytest.fixture(scope="session", autouse=True)
+def create_test_db():
+    async def create():
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+
+    asyncio.run(create())
